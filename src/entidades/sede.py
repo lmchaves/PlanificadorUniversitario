df2f7f21c422e1a9ff2ca49e08f529ed7b07a6dc
evaanngiil <evaanngc@gmail.com>
2024-11-14 20:20:37 +0100

    feat(Sede): Inventario es un diccionario

--- src/entidades/sede.py
+++ src/entidades/sede.py
@@ -4,12 +4,13 @@ class Sede:
     
     Atributos:
       sede_id (int): Identificador único de la sede
+      nombre (str): Nombre descriptivo de la sede
       ubicacion (str): Ubicación geográfica de la sede
-      inventario (list): Lista de piezas disponibles en la sede
+      inventario (dict): Diccionario que almacena las piezas y sus cantidades
     """
-    def __init__(self, sede_id: int, ubicacion: str, inventario: list):
+    def __init__(self, sede_id: int, nombre: str , ubicacion: str, inventario: dict):
         """Inicializa una nueva instancia de la clase Sede"""
         self.sede_id = sede_id
+        self.nombre = nombre
         self.ubicacion = ubicacion
-        self.inventario = inventario
-
+        self.inventario = inventario or {}  # Inicializa el inventario como un diccionario vacío si no se proporciona
