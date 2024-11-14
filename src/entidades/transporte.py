8f43af6c82a86fbf2ed2a4464132be4ed401838d
evaanngiil <evaanngc@gmail.com>
2024-11-14 20:54:33 +0100

    docs(Tranporte): Resuelve #13 - Añade descripción de los tipos de transporte y como son elegidos

--- src/entidades/transporte.py
+++ src/entidades/transporte.py
@@ -1,19 +1,8 @@
-from enum import Enum
-
-class TipoTransporte(Enum):
-    """
-    Representa los tipos de transporte disponibles para el traslado de piezas.
-    """
-    URGENTE = 1
-    ESTANDAR = 2
-    ECONOMICO = 3
-
 class Transporte:
     """
     Representa los tipos de transporte disponibles para el traslado de piezas.
 
     Atributos:
-      tipo (TipoTransporte): Tipo de transporte
       costo_por_km (float): Costo en euros por kilómetro de transporte
       tiempo_por_km (float): Tiempo en horas por kilómetro de transporte
         
@@ -21,10 +10,9 @@ class Transporte:
       - costo_por_km debe ser mayor que cero
       - tiempo_por_km debe ser mayor que cero
     """
-    def __init__(self, tipo: TipoTransporte, costo_por_km: float, tiempo_por_km: float):
+    def __init__(self, costo_por_km: float, tiempo_por_km: float):
         """
         Inicializa una nueva instancia de la clase Transporte
         """
-        self.tipo = tipo
         self.costo_por_km = max(0.01, costo_por_km)  # Restricción: costo > 0
         self.tiempo_por_km = max(0.01, tiempo_por_km)  # Restricción: tiempo por km > cero
