a08080070dbb1a610efd5709f932f32920cd3a65
evaanngiil <evaanngc@gmail.com>
2024-11-26 18:33:53 +0100

    Feat(Pieza): elimina el arhcivo del objeto valor pieza_caracterisitica, ya que se incluye en el mismo fichero de la clase pieza al no utilizarse. -Resuelve #11

--- src/pieza.py
+++ src/pieza.py
@@ -1,5 +1,23 @@
 from dataclasses import dataclass
-from pieza_caracteristica import PiezaCaracteristica
+
+@dataclass(frozen=True)
+class PiezaCaracteristica:
+    """
+    Representa una pieza específica dentro del inventario de una sede.
+
+    Atributos:
+        descripcion (str): Descripción detallada de la pieza.
+        categoria_costo (dict): Diccionario que relaciona la categoría con el costo unitario de la pieza.
+
+    Restricciones:
+        - costo debe ser positivo
+    """
+    descripcion: str
+    categoria_costo: dict[str, float]
+
+    def __post_init__(self):
+        if not self.descripcion or not self.categoria_costo:
+            raise ValueError("Descripción, categoría_costo y tipo de vehículo no pueden estar vacíos.")
 
 @dataclass
 class PiezaInventario:
