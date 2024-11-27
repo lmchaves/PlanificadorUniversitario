4ffbf66533cdfc146c42898c799981dee5864545
evaanngiil <evaanngc@gmail.com>
2024-11-27 19:24:38 +0100

    Feat(Pieza): Resuelve #11 - Reestructura el código para definir el objeto valor Pieza, separado en una clase ya que se utiliza para resolver #12 y #10

--- src/pieza.py
+++ src/pieza.py
@@ -1,13 +1,15 @@
 from dataclasses import dataclass
 
 @dataclass(frozen=True)
-class PiezaCaracteristica:
+class Pieza:
     """
-    Representa una pieza específica dentro del inventario de una sede.
+    Representa una pieza específica dentro del inventario de una sede. Objeto valor
 
     Atributos:
         descripcion (str): Descripción detallada de la pieza.
         categoria_costo (dict): Diccionario que relaciona la categoría con el costo unitario de la pieza.
+        En un diccionario ya que la clave categoría debe tener un solo un costo inmutable en el inventario.
+        Un objeto valor será igual a otro si sus atributos son iguales.
 
     Restricciones:
         - costo debe ser positivo
@@ -17,24 +19,10 @@ class PiezaCaracteristica:
 
     def __post_init__(self):
         if not self.descripcion or not self.categoria_costo:
-            raise ValueError("Descripción, categoría_costo y tipo de vehículo no pueden estar vacíos.")
+            raise ValueError("Descripción y categoría_costo no pueden estar vacíos.")
 
-@dataclass
-class PiezaInventario:
-    """
-    Entidad que representa una pieza en el inventario de una sede (entidad).
-
-    Atributos:
-        caracteristicas (PiezaCaracteristica): Características de la pieza (objeto valor).
-        cantidad (int): Cantidad disponible en el inventario.
-        nomnbre_sede (str): Ubicación actual de la pieza.
-    """
-    caracteristicas: PiezaCaracteristica
-    cantidad: int
-    nombre_sede: str
-
-    def __post_init__(self):
-        if self.cantidad < 0:
-            raise ValueError("La cantidad no puede ser negativa.")
-        if not self.nombre_sede:
-            raise ValueError("La ubicación no puede estar vacía.")
+        for categoria, costo in self.categoria_costo.items():
+            if not isinstance(categoria, str):
+                raise ValueError("La categoría debe ser una cadena de texto.")
+            if costo <= 0:
+                raise ValueError("El costo debe ser mayor que cero.")
