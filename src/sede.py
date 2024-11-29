d22d792177d2ea676abe99ed1995c18d4f129ad5
evaanngiil <evaanngc@gmail.com>
2024-11-29 14:04:43 +0100

    feat(Sede):Añade inventario con clave objeto Pieza  - Resuelve ##10 y #19

--- src/sede.py
+++ src/sede.py
@@ -5,19 +5,18 @@ from pieza import Pieza
 class Sede:
     """
     Representa una sede del taller, con información de ubicación y detalles del inventario de piezas.
-    
+
     Atributos:
         nombre (str): Nombre de la sede.
         ubicacion_latitude (float): Latitud de la ubicación de la sede.
         ubicacion_longitude (float): Longitud de la ubicación de la sede.
-        catalogo (list[Pieza]): Catálogo de piezas que se pueden solicitar en esta sede.
-        inventario (dict[str, int]): Diccionario que mapea nombres de piezas a sus cantidades disponibles.
+        inventario (dict[Pieza, int]): Diccionario que mapea el objeto pieza y su cantidad.
     """
     nombre: str
     ubicacion_latitude: float
     ubicacion_longitude: float
-    catalogo: list[Pieza]
-    inventario: dict[str, int]
+    inventario: dict[Pieza, int]
+    # inventario (dict[str, tuple[int, float]]):
 
     def __post_init__(self):
         if not isinstance(self.inventario, dict):
@@ -30,9 +29,6 @@ class Sede:
             raise ValueError("La longitud debe estar en el rango [-180, 180].")
 
         # Validación del inventario: las cantidades deben ser no negativas.
-        for pieza, cantidad in self.inventario.items():
-            if not isinstance(pieza, Pieza):
-                raise ValueError("El inventario debe contener solo nombres de pieza como claves.")
+        for pieza, (cantidad, _) in self.inventario.items():
             if cantidad < 0:
-                raise ValueError("Las cantidades en el inventario no pueden ser negativas.")
-
+                raise ValueError("Las cantidades de la pieza " + pieza + " en el inventario no pueden ser negativas.")
