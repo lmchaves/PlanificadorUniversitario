fc1795cd5a2ff4c1b4625405a52dd0f580f012d4
lmchaves <lmchaveslop@gmail.com>
2025-01-17 17:53:22 +0100

    fix #27: elimno datos sedes
    para lso datos de pureba lo realizo mediante un fixture

--- tests/test_pedidos.py
+++ tests/test_pedidos.py
@@ -1,9 +1,48 @@
 import pytest
 from pedido import seleccionar_sede_optima
-from datos_sedes import sede_malaga, sede_cordoba, sede_jaen, sede_principal
+from sede  import Sede
 
 @pytest.fixture
 def setup_data():
+    inventario_malaga = {
+        "Motor": (5, 3100),
+        "Correa de distribución": (3, 75),
+    }
+    inventario_cordoba = {
+        "Motor": (10, 3050),
+        "Alternador": (4, 240),
+    }
+    inventario_jaen = {
+        "Alternador": (5, 3150),
+        "Correa de distribución": (2, 70),
+    }
+    inventario_principal = {}
+
+    sede_malaga = Sede(
+        nombre="Málaga",
+        ubicacion_latitude=36.721,
+        ubicacion_longitude=-4.421,
+        inventario=inventario_malaga,
+    )
+    sede_cordoba = Sede(
+        nombre="Córdoba",
+        ubicacion_latitude=37.888,
+        ubicacion_longitude=-4.779,
+        inventario=inventario_cordoba,
+    )
+    sede_jaen = Sede(
+        nombre="Jaén",
+        ubicacion_latitude=37.779,
+        ubicacion_longitude=-3.784,
+        inventario=inventario_jaen,
+    )
+    sede_principal = Sede(
+        nombre="Principal",
+        ubicacion_latitude=37.000,
+        ubicacion_longitude=-4.500,
+        inventario=inventario_principal,
+    )
+
     piezas_requeridas = {
         "Motor": 3,
         "Alternador": 2,
@@ -11,7 +50,6 @@ def setup_data():
     }
 
     return [sede_malaga, sede_cordoba, sede_jaen], sede_principal, piezas_requeridas
-
 
 def test_seleccionar_sede_optima_por_pieza(setup_data):
     sedes, sede_principal, piezas_requeridas = setup_data
@@ -30,7 +68,7 @@ def test_seleccionar_sede_optima_por_pieza(setup_data):
     pedido_alternador = next((p for p in resultado if "Alternador" in p.piezas_requeridas), None)
     assert pedido_alternador is not None,  "No se encontró el pedido para 'Alternador'"
     assert pedido_alternador.piezas_requeridas["Alternador"] == 2,f"Se esperaba 3 alnernadores, pero obtuve {pedido_alternador.piezas_requeridas['Alternador']}"
-    assert pedido_alternador.nombre_sede_destino in [ "Jaén"], f"Se esperaba que la sede fuera 'Jaén', pero obtuve {pedido_alternador.nombre_sede_destino}" 
+    assert pedido_alternador.nombre_sede_destino in [ "Córdoba"], f"Se esperaba que la sede fuera 'Córdoba', pero obtuve {pedido_alternador.nombre_sede_destino}" 
     
 
     pedido_Correa = next((p for p in resultado if "Correa de distribución" in p.piezas_requeridas), None)
