86260c44be385946229bff38beee3097538a9202
lmchaves <lmchaveslop@gmail.com>
2025-01-17 17:45:35 +0100

    fix #27: arreglo los test
    ya que hemos cambiado tanto trealizar pedido como seleccionar la sede

--- tests/test_pedidos.py
+++ tests/test_pedidos.py
@@ -1,5 +1,5 @@
 import pytest
-from pedido import seleccionar_sede_optima, realizar_pedido, Pedido
+from pedido import seleccionar_sede_optima
 from datos_sedes import sede_malaga, sede_cordoba, sede_jaen, sede_principal
 
 @pytest.fixture
@@ -20,33 +20,33 @@ def test_seleccionar_sede_optima_por_pieza(setup_data):
 
     assert len(resultado) == 3, f"Se esperaba 3 piezas, pero obtuve {len(resultado)}"
 
-    pedido_motor = next((p for p in resultado if p["pieza"] == "Motor"), None)
+    pedido_motor = next((p for p in resultado if "Motor" in p.piezas_requeridas), None)
     assert pedido_motor is not None, "No se encontró el pedido para 'Motor'"
-    assert pedido_motor["cantidad"] == 3, f"Se esperaba 3 motores, pero obtuve {pedido_motor['cantidad']}"
-    assert pedido_motor["sede"] == "Córdoba", f"Se esperaba que la sede fuera 'Córdoba', pero obtuve {pedido_motor['sede']}"
+    assert pedido_motor.piezas_requeridas["Motor"] == 3, f"Se esperaba 3 motores, pero obtuve {pedido_motor.piezas_requeridas['Motor']}"
+    assert pedido_motor.nombre_sede_destino == "Córdoba", f"Se esperaba que la sede fuera 'Córdoba', pero obtuve {pedido_motor.nombre_sede_destino}"
 
 
 
-    pedido_alternador = next((p for p in resultado if p["pieza"] == "Alternador"), None)
+    pedido_alternador = next((p for p in resultado if "Alternador" in p.piezas_requeridas), None)
     assert pedido_alternador is not None,  "No se encontró el pedido para 'Alternador'"
-    assert pedido_alternador["cantidad"] == 2,f"Se esperaba 3 alnernadores, pero obtuve {pedido_alternador['cantidad']}"
-    assert pedido_alternador["sede"] in [ "Jaén"], f"Se esperaba que la sede fuera 'Jaén', pero obtuve {pedido_alternador['sede']}" 
+    assert pedido_alternador.piezas_requeridas["Alternador"] == 2,f"Se esperaba 3 alnernadores, pero obtuve {pedido_alternador.piezas_requeridas['Alternador']}"
+    assert pedido_alternador.nombre_sede_destino in [ "Jaén"], f"Se esperaba que la sede fuera 'Jaén', pero obtuve {pedido_alternador.nombre_sede_destino}" 
     
 
-    pedido_Correa = next((p for p in resultado if p["pieza"] == "Correa de distribución"), None)
+    pedido_Correa = next((p for p in resultado if "Correa de distribución" in p.piezas_requeridas), None)
     assert pedido_Correa is not None,"No se encontró el pedido para 'Correa de distribución'"
-    assert pedido_Correa["cantidad"] == 1,f"Se esperaba 1 correa de distribución, pero obtuve {pedido_Correa['cantidad']}"
-    assert pedido_Correa["sede"] in ["Málaga"], f"Se esperaba que la sede fuera 'Málaga', pero obtuve {pedido_Correa['sede']}" 
+    assert pedido_Correa.piezas_requeridas["Correa de distribución"] == 1,f"Se esperaba 1 correa de distribución, pero obtuve {pedido_Correa.piezas_requeridas['Correa de distribución']}"
+    assert pedido_Correa.nombre_sede_destino in ["Málaga"], f"Se esperaba que la sede fuera 'Málaga', pero obtuve {pedido_Correa.nombre_sede_destino}" 
 
 
-def test_calcular_costos_distancias(setup_data):
+def test_calcular_costos_hora(setup_data):
     sedes, sede_principal, piezas_requeridas = setup_data
 
     resultado = seleccionar_sede_optima(piezas_requeridas, sedes, sede_principal)
 
     for pedido in resultado:
-        assert pedido["costo_total"] > 0, "El costo total debe ser mayor a cero"
-        assert pedido["distancia"] > 0, "La distancia debe ser mayor a cero"
+        assert pedido.costo_transporte > 0, "El costo total debe ser mayor a cero"
+        assert pedido.tiempo_estimado_llegada > 0, "La hora debe ser mayor a cero"
 
 
 def test_piezas_no_disponibles(setup_data):
